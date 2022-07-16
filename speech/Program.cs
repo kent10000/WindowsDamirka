dbfc48dbc74a81f66e54127ff3d312d6435670da
kent10000 <aaronrichards4321@gmail.com>
2022-07-16 19:01:33 -0400

    Initial commit

---
+++ speech/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using speech;
+
+#pragma warning disable CA1416
+//Only works on Windows
+
+var itemList = new TarkovTool("items", new[] { "shortName" });
+
+var list = await itemList.GetResponse();
+
+if (list == null)
+{
+    throw new HttpRequestException();
+}
+
+//remove every \" from the list
+list = list.Select(x => x.Replace("\"", "")).ToArray();
+
+
+
+var c = new Choices(list);
+
+
+
+var items = new GrammarBuilder(c);
+
+var price = new GrammarBuilder("price");
+price.Append(items);
+
+
+
+var grammar = new Grammar(price);
+
+var engine = new SpeechRecognizer();
+engine.LoadGrammar(grammar);
+
+var speech = new SpeechSynthesizer();
+
+var priceGetter = new TarkovTool("items", new []{"avg24hPrice"});
+
+engine.SpeechRecognized += (async (_, eventArgs) =>
+{
+    var t = eventArgs.Result.Words[1].Text;
+    //Console.WriteLine(t);
+    var itemPrice = await priceGetter.GetResponse(new KeyValuePair<string, string>("name", t));
+    if (itemPrice == null || itemPrice.Length == 0)
+    {
+        speech.SpeakAsync("Item not found");
+        return;
+    } 
+    if (itemPrice[0] == "0")
+    {
+        speech.SpeakAsync(t + " is not for sale on the flea market");
+        return;
+    }
+    speech.SpeakAsync("The price of " + t + " is " + itemPrice[0] + " roubles");
+    
+    
+});
+//for testing api
+/*
+var levels = new TarkovTool("playerLevels",new[]{"level", "exp"});
+
+var l = await levels.GetResponse();
+
+foreach (var i in l)
+{
+    Console.WriteLine(i);
+}*/
+
+
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
---
+++ speech/TarkovTool.cs
@@ -0,0 +1,143 @@
+﻿using System.Net.Http.Json;
+using Newtonsoft.Json.Linq;
+
+namespace speech;
+
+public class TarkovTool
+{
+    private readonly string _query;
+    private readonly string[] _details;
+    private readonly HttpClient _httpClient;
+
+    public TarkovTool(string query, string[] details)
+    {
+        _query = query;
+        _details = details;
+        _httpClient = new HttpClient();
+        _httpClient.BaseAddress = new Uri("https://api.tarkov.dev/graphql");
+    }
+    
+    public async Task<string[]?> GetResponse(KeyValuePair<string, string> args)
+    {
+        var responses = new List<string>();
+
+
+        var arguments = args.Key[^1] == 's' ? $"({args.Key}: {args.Value})" : $"({args.Key}: \"{args.Value}\")";
+
+
+        /*if (_args is not string)
+        {
+            arguments = arguments = $"({_args.Key}: {_args.Value})";
+        } else
+       {
+                
+        }
+            arguments = $"({_args.Key}: {_args.Value})";*/
+
+        var details = _details.Aggregate<string?, string?>(null, (current, detail) => current + $"{detail} ");
+
+        details = details?.TrimEnd();
+
+        var q = "{" + _query + arguments + "{" + details + "}}";
+
+        /*var request = new GraphQLRequest
+        {
+            Query = q
+        };*/
+
+        //var r = client.SendQueryAsync<>(request);
+
+        //var responses = await client.SendQueryAsync<dynamic>(request);
+        var response = await _httpClient.PostAsJsonAsync("", new Dictionary<string, string> { { "query", q } });
+
+        var jsonString = await response.Content.ReadAsStringAsync();
+
+        var json = JObject.Parse(jsonString);
+        //var value = json.TryGetValue("item", out var name);
+        //json.First.ToString();
+        //Console.WriteLine(json.First.First.Type.ToString());
+
+
+        if (json.First == null) return responses.Count == 0 ? null : responses.ToArray();
+        if (json.First.First == null) return responses.Count == 0 ? null : responses.ToArray();
+        if (json.First.First.First == null) return responses.Count == 0 ? null : responses.ToArray();
+        var result = json.First.First.First.First;
+        if (result == null) return null;
+        if (result.Type == JTokenType.Array)
+        {
+            responses.AddRange(from item in result
+                from child in item.Children()
+                from baby in child.Children()
+                select baby.ToString());
+        }
+        else
+        {
+            var children = result.Children();
+            responses.AddRange(from child in children from baby in child.Children() select baby.ToString());
+        }
+
+        return responses.Count == 0 ? null : responses.ToArray();
+    }
+    
+    public async Task<string[]?> GetResponse()
+    {
+        var responses = new List<string>();
+
+
+        //var arguments = args.Key[^1] == 's' ? $"({args.Key}: {args.Value})" : $"({args.Key}: \"{args.Value}\")";
+
+
+        /*if (_args is not string)
+        {
+            arguments = arguments = $"({_args.Key}: {_args.Value})";
+        } else
+       {
+                
+        }
+            arguments = $"({_args.Key}: {_args.Value})";*/
+
+        var details = _details.Aggregate<string?, string?>(null, (current, detail) => current + $"{detail} ");
+
+        details = details?.TrimEnd();
+
+        var q = "{" + _query +  "{" + details + "}}";
+
+        /*var request = new GraphQLRequest
+        {
+            Query = q
+        };*/
+
+        //var r = client.SendQueryAsync<>(request);
+
+        //var responses = await client.SendQueryAsync<dynamic>(request);
+        var response = await _httpClient.PostAsJsonAsync("", new Dictionary<string, string> { { "query", q } });
+
+        var jsonString = await response.Content.ReadAsStringAsync();
+
+        var json = JObject.Parse(jsonString);
+        //var value = json.TryGetValue("item", out var name);
+        //json.First.ToString();
+        //Console.WriteLine(json.First.First.Type.ToString());
+
+
+        if (json.First == null) return responses.Count == 0 ? null : responses.ToArray();
+        if (json.First.First == null) return responses.Count == 0 ? null : responses.ToArray();
+        if (json.First.First.First == null) return responses.Count == 0 ? null : responses.ToArray();
+        var result = json.First.First.First.First;
+        if (result == null) return null;
+        if (result.Type == JTokenType.Array)
+        {
+            responses.AddRange(from item in result
+                from child in item.Children()
+                from baby in child.Children()
+                select baby.ToString());
+        }
+        else
+        {
+            var children = result.Children();
+            responses.AddRange(from child in children from baby in child.Children() select baby.ToString());
+        }
+
+        return responses.Count == 0 ? null : responses.ToArray();
+    }
+}
